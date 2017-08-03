b45b859d03fb339623f010b096151c3acb772b4c
Tara Vancil <tbvanc@gmail.com>
2017-08-03 14:15:27 -0500

    Update the follow/following/unfollow button component

--- js/com/follow-btn.js
+++ js/com/follow-btn.js
@@ -3,15 +3,29 @@ const html = require('choo/html')
 module.exports = function renderFollowBtn (state, emit, profile) {
   if (profile.isFollowed) {
     return html`
-      <div><a onclick=${toggleFollow}><i class="fa fa-user-times"></i> Unfollow</a></div>
+      <button id="follow-toggle" class="btn success outline" onmouseout=${onShowFollowingButton} onmouseover=${onShowUnfollowButton} onclick=${toggleFollow}>Following<i class="fa fa-check"></i></button>
     `
   } else {
     return html`
-      <div><a onclick=${toggleFollow}><i class="fa fa-user-plus"></i> Follow</a></div>
+      <button id="follow-toggle" class="btn primary outline" onclick=${toggleFollow}>Follow<i class="fa fa-plus"></i></button>
     `
   }
 
   function toggleFollow () {
     state.toggleFollow(profile)
+  }
+
+  function onShowUnfollowButton () {
+    const btn = document.getElementById('follow-toggle')
+    btn.innerHTML = 'Unfollow<i class="fa fa-times"></i>'
+    btn.classList.remove('success')
+    btn.classList.add('destructive')
+  }
+
+  function onShowFollowingButton () {
+    const btn = document.getElementById('follow-toggle')
+    btn.innerHTML = 'Following<i class="fa fa-check"></i>'
+    btn.classList.remove('destructive')
+    btn.classList.add('success')
   }
 }
