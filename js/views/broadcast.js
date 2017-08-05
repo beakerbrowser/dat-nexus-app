fabc7163b0193d6f7fc44c12ecb62fc27840bf0a
Tara Vancil <tbvanc@gmail.com>
2017-08-05 18:47:58 -0500

    Update broadcast view

--- js/views/broadcast.js
+++ js/views/broadcast.js
@@ -3,8 +3,10 @@
 const html = require('choo/html')
 const loadingView = require('./loading')
 const renderError = require('../com/error')
+const renderHeader = require('../com/header')
 const renderBroadcast = require('../com/broadcast')
 const renderProfile = require('../com/profile-card')
+const {getViewProfileURL} = require('../util')
 
 module.exports = function broadcastView (state, emit) {
   if (!state.isAppLoaded) {
@@ -13,18 +15,20 @@ module.exports = function broadcastView (state, emit) {
   if (!state.currentBroadcast) {
     state.loadCurrentBroadcast('dat://' + state.params.wildcard)
     return loadingView(state, emit)
+  } else {
+    var author = state.currentBroadcast.author
   }
   return html`
     <main>
-      <div class="grid">
-        <div class="feed-container">
+      ${renderHeader(state, emit, state.userProfile)}
+      <div class="main-container">
+        <div class="main-content center">
           ${renderError(state, emit)}
-          <p><a href="#"><i class="fa fa-caret-left"></i> Back to feed</a></p>
-          <h2>${state.currentBroadcast.author.name + "'"}s broadcast</h2>
+          <a href=${getViewProfileURL(author)} class="breadcrumbs">
+            <i class="fa fa-caret-left"></i>
+            Back to ${author.name}'s feed
+          </a>
           ${renderBroadcast(emit, state.currentBroadcast)}
-        </div>
-        <div class="sidebar">
-          ${renderProfile(state, emit, state.userProfile)}
         </div>
       </div>
     </main>
