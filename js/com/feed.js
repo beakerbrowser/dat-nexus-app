4fb7982d8f8e60bd4911411348a68b9b0989bf21
Tara Vancil <tbvanc@gmail.com>
2017-08-05 19:57:32 -0500

    Update empty feed styles and content

--- js/com/feed.js
+++ js/com/feed.js
@@ -4,21 +4,13 @@ const renderBroadcast = require('./broadcast')
 module.exports = function renderFeed (state, emit) {
   if (!state.broadcasts) {
     return html`
-      <p class="feed">
-        Loading...
-      </p>
-    `
-  } else if (state.broadcasts.length) {
-    return html`
-      <ul class="feed">
-        ${state.broadcasts.map(b => renderBroadcast(emit, b))}
-      </ul>
-    `
-  } else {
-    return html`
-      <p class="feed">
-        No broadcasts.
+      <p class="card">
+        <i class="fa fa-spinner"></i> Loading feed...
       </p>
     `
   }
+
+  return html`
+    <ul class="feed">${state.broadcasts.map(b => renderBroadcast(emit, b))}</ul>
+  `
 }
